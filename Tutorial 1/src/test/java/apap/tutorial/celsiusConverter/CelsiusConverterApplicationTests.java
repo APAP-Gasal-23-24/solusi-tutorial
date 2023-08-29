d0d51ef02a78e497da8e3342d49a2952af322d13
Muhammad Rafi Atha Zhafran <muhammad.rafi07@ui.ac.id>
2023-08-29 20:43:20 +0700

    solusi tutorial 1

---
+++ Tutorial 1/src/test/java/apap/tutorial/celsiusConverter/CelsiusConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.tutorial.celsiusConverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelsiusConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
